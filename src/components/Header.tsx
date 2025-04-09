c8bca37191d99b7728e956896445d9f160da7dfb
Pedro Henrique <pedrohenriquebsouza.1@gmail.com>
2025-04-08 22:27:33 -0300

    Adicionando tipagens e ocss no Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,16 @@
-export function Header() {
+import React from "react";
+import style from "./Header.module.css"
+
+type HeaderProps = {
+    children: React.ReactNode;
+}
+
+export function Header({ children }: HeaderProps) {
     return (
-        <h1>
-            ola
-        </h1>
+       <>
+       <h1 className={style.header}>
+         {children}
+       </h1>
+       </>
     )
 }
