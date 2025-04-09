eced0545a44929db165a4613060365778708f519
Pedro Henrique <pedrohenriquebsouza.1@gmail.com>
2025-04-09 09:55:03 -0300

    Iniciar o app

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
 import "./global.css"
 import "./theme.css"
 
-import { TimerIcon } from "lucide-react"
-import { Header } from "./components/Header"
 
 function App() {
   return (
     <>
-      <Header>
-        <h1>ola mundo</h1>
-        <button>
-          <TimerIcon/>
-        </button>
-      </Header>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex asperiores labore numquam repellat tempore dolorem, unde veritatis eum, iste, deleniti libero impedit. Est aperiam repellendus soluta aut voluptas sit nesciunt?
-      </p>
+      <div className="container">
+        <div className="content">
+          <section>
+            LOGO
+          </section>
+        </div>
+      </div>
     </>
   )
 }
