bbb4a5c2c98b4e21a1fc1d91121f63985b5b9e62
Pedro Henrique <pedrohenriquebsouza.1@gmail.com>
2025-04-08 22:31:24 -0300

    Adicionado o children do Header e o css global do site

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import "./global.css"
+import "./theme.css"
+
+import { TimerIcon } from "lucide-react"
 import { Header } from "./components/Header"
 
 function App() {
   return (
     <>
-      <Header/>
+      <Header>
+        <h1>ola mundo</h1>
+        <button>
+          <TimerIcon/>
+        </button>
+      </Header>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex asperiores labore numquam repellat tempore dolorem, unde veritatis eum, iste, deleniti libero impedit. Est aperiam repellendus soluta aut voluptas sit nesciunt?
+      </p>
     </>
   )
 }
