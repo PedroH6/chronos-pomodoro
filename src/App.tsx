e7251e06750f151e707d4c3cc3427f69e4b36a1e
Pedro Henrique <pedrohenriquebsouza.1@gmail.com>
2025-04-09 15:20:34 -0300

    Criando o formulario da pagina inicial

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,28 @@ function App() {
       <Container>
         <CountDown/>
       </Container>
+
+      <Container>
+       <form action="" className="form">
+        <div className="fromRow">
+          <label htmlFor="">task</label>
+          <input id="input" type="text" />
+        </div>
+
+        <div className="fromRow">
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+
+        <div className="fromRow">
+          <p>Ciclos</p>
+          <p>0 0 0 0 0 0  </p>
+        </div>
+
+        <div className="fromRow">
+          <button>Enviar</button>
+        </div>
+       </form>
+      </Container>
     </>
   )
 }
