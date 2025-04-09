72751b79340120fd63f38da3355d7b0b17e51eee
Pedro Henrique <pedrohenriquebsouza.1@gmail.com>
2025-04-09 11:42:00 -0300

    Criando o component de CountDown

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Container } from "./components/Container"
+import { CountDown } from "./components/CountDown"
 import { Logo } from "./components/Logo"
 import { Menu } from "./components/Menu"
 
@@ -16,6 +17,10 @@ function App() {
 
       <Container>
         <Menu />
+      </Container>
+
+      <Container>
+        <CountDown/>
       </Container>
     </>
   )
