73604ba611fbf4b6425a19ad6d48c79adda3c4a0
Pedro Henrique <pedrohenriquebsouza.1@gmail.com>
2025-04-10 16:01:45 -0300

    Criando a pagina Home e o template para todas as outras paginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,13 @@
-import { PlayCircleIcon } from "lucide-react"
-import { Button } from "./components/Button"
-import { Container } from "./components/Container"
-import { CountDown } from "./components/CountDown"
-import { Cycles } from "./components/Cycles"
-import { Input } from "./components/Input"
-import { Logo } from "./components/Logo"
-import { Menu } from "./components/Menu"
-import { Footer } from "./components/Footer"
-
+import { Home } from "./pages/Home"
 
 import "./global.css"
 import "./theme.css"
 
 
-
 function App() {
   return (
     <>
-      <Container>
-        <Logo />
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <CountDown/>
-      </Container>
-
-      <Container>
-       <form action="" className="form">
-        <div className="fromRow">
-          <Input 
-            id="meuInput" 
-            type="text" 
-            placeholder="digite algo"
-            defaultValue="valor prenchido"
-            />
-        </div>
-
-        <div className="fromRow">
-          <p>Lorem ipsum dolor sit amet.</p>
-        </div>
-
-        <div className="fromRow">
-         <Cycles />
-        </div>
-
-        <div className="fromRow">
-         <Button icon={<PlayCircleIcon/>} color="green"/>
-        </div>
-       </form>
-      </Container>
-
-      <Container>
-        <Footer/>
-      </Container>
+    <Home />
     </>
   )
 }
---
+++ src/templates/MainTemplates/index.tsx
@@ -0,0 +1,36 @@
+import React from "react"
+import { Container } from "../../components/Container"
+import { Footer } from "../../components/Footer"
+import { Logo } from "../../components/Logo"
+import { Menu } from "../../components/Menu"
+
+import "./global.css"
+import "./theme.css"
+
+
+type MainTemplateProps = {
+    children: React.ReactNode
+}
+
+
+export function MainTemplate({ children }: MainTemplateProps) {
+  return (
+    <>
+      <Container>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Menu />
+      </Container>
+
+      {children}
+      
+      <Container>
+        <Footer/>
+      </Container>
+    </>
+  )
+}
+
+
