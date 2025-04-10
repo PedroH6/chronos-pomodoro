c8c6a7e74f22a3e25ba67ded0b7b749e4d036906
Pedro Henrique <pedrohenriquebsouza.1@gmail.com>
2025-04-10 16:10:09 -0300

    Corrigindo error de css

--- src/templates/MainTemplates/index.tsx
+++ src/templates/MainTemplates/index.tsx
@@ -4,8 +4,8 @@ import { Footer } from "../../components/Footer"
 import { Logo } from "../../components/Logo"
 import { Menu } from "../../components/Menu"
 
-import "./global.css"
-import "./theme.css"
+import "../../global.css"
+import "../../theme.css"
 
 
 type MainTemplateProps = {
