1673c40b4407beffd134065c0785f02a7fe164a3
Pedro Henrique <pedrohenriquebsouza.1@gmail.com>
2025-04-15 22:08:56 -0300

    Validando os campos do formulário antes de salvar os dados

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -6,6 +6,7 @@ import { SaveIcon } from 'lucide-react';
 import { MainTemplate } from '../../templates/MainTemplates';
 import { useTaskContext } from '../../contexts/TaskContext/userTaskContext';
 import { useRef } from 'react';
+import { showMessage } from '../../components/adapters/showMessage';
  
  export function Settings() {
    const { state } = useTaskContext();
@@ -16,13 +17,42 @@ import { useRef } from 'react';
 
     function handleSaveSettings(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault();
+        showMessage.dismiss();
+
+        const formErrors = [];
+
+        const workTime = Number(workTimeInput.current?.value);
+        const shortBreakTime = Number(shortBreakTimeInput.current?.value);
+        const longBreakTime = Number(longBreakTimeInput.current?.value);
+        
     
-        const workTime = workTimeInput.current?.value;
-        const shortBreakTime = shortBreakTimeInput.current?.value;
-        const longBreakTime = longBreakTimeInput.current?.value;
+        if (isNaN(workTime) || isNaN(shortBreakTime) || isNaN(longBreakTime)) {
+          formErrors.push('Digite apenas números para TODOS os campos');
+        }
     
-        console.log(workTime, shortBreakTime, longBreakTime);
+        if (workTime < 1 || workTime > 99) {
+          formErrors.push('Digite valores entre 1 e 99 para foco');
+        }
+    
+        if (shortBreakTime < 1 || shortBreakTime > 30) {
+          formErrors.push('Digite valores entre 1 e 30 para descanso curto');
+        }
+    
+        if (longBreakTime < 1 || longBreakTime > 60) {
+          formErrors.push('Digite valores entre 1 e 60 para descanso longo');
+        }
+    
+        if (formErrors.length > 0) {
+          formErrors.forEach(error => {
+            showMessage.error(error);
+          });
+          return;
+        }
+    
+        console.log('SALVAR');
       }
+
+  
 
    return (
      <MainTemplate>
@@ -45,6 +75,7 @@ import { useRef } from 'react';
                labelText='Foco'
                ref={workTimeInput}
                defaultValue={state.config.workTime}
+               type='number'
                />
            </div>
            <div className='formRow'>
@@ -53,6 +84,7 @@ import { useRef } from 'react';
                 labelText='Descanso curto' 
                 ref={shortBreakTimeInput}
                 defaultValue={state.config.shortBreakTime}
+                type='number'
                 />
            </div>
            <div className='formRow'>
@@ -60,7 +92,8 @@ import { useRef } from 'react';
                id='longBreakTime' 
                labelText='Descanso longo'
                ref={longBreakTimeInput}
-               defaultValue={state.config.longBreakTime} 
+               defaultValue={state.config.longBreakTime}
+               type='number'
                />
            </div>
            <div className='formRow'>
